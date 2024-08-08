87bbf8c179f67fd573f924d1baebe4fa018a620f
Stefan Jenss <stefanjenss2025@u.northwestern.edu>
2024-08-07 20:36:03 -0400

    Running into "A KerasTensor is sympolic" error. Will try running in a Google colab, but may have to abondom pre-experiment 2.

--- V1_Pre-Experiment_2.py
+++ V1_Pre-Experiment_2.py
@@ -91,9 +91,109 @@ train_df, remaining = train_test_split(df, test_size=0.3, stratify=df['label'], 
 
 # Second Split: 50% of the remaining data for validation, the other 50% for testing
 val_df, test_df = train_test_split(remaining, test_size=0.5, stratify=remaining['label'], random_state=42)
-# %%
+
 # Check the shape of the training, validation, and test sets
 print(f"Training Dataset Shape: {train_df.shape}")
 print(f"Validation Dataset Shape: {val_df.shape}")
 print(f"Test Dataset Shape: {test_df.shape}")
+
+# %%
+# 5. Create Tensorflow Dataset
+# -------------------------------------------------------------------------------------------------
+"""
+We will define the function `create_dataset` to create a Tensorflow Dataset from the cleaned review
+text and label columns of the dataframe. 
+
+The function is used to create three Tensorflow Datasets (train_ds,  val_ds, and test_ds) from the
+respective subsets of the cleaned review text and label columns.
+"""
+def create_dataset(dataframe, shuffle=True, batch_size=32):
+    """
+    Create a TensorFlow Dataset from the cleaned review text and label columns of the given dataframe.
+
+    Args:
+        dataframe (pandas.DataFrame): The dataframe containing the cleaned review text and label columns.
+        shuffle (bool, optional): Whether to shuffle the dataset. Defaults to True.
+        batch_size (int, optional): The batch size for the dataset. Defaults to 32.
+
+    Returns:
+        tf.data.Dataset: The TensorFlow Dataset created from the dataframe.
+    """
+    ds = tf.data.Dataset.from_tensor_slices((dataframe['cleaned_review_text'].values, dataframe['label'].values))
+    if shuffle:
+        ds = ds.shuffle(buffer_size=len(dataframe))
+    ds = ds.batch(batch_size)
+    ds = ds.prefetch(tf.data.AUTOTUNE)
+    return ds
+
+# Create the Tensorflow Datasets
+train_ds = create_dataset(train_df)
+val_ds = create_dataset(val_df)
+test_ds = create_dataset(test_df)
+
+# %%
+# 6. Load the BERT pre-processing model and encoder
+# -------------------------------------------------------------------------------------------------
+"""
+Pre-processing model:
+    - We are using a bert_en_uncased preprocessing module for the BERT model.
+    - This is used to pre-process the input text data, which will include tokenization,
+      conversion to lowercase, and generation of special tokens, like [CLS] and [SEP]--
+      which are required for the BERT model.
+
+Encoder:
+    - We are also using a bert_en_uncase encoder module for the BERT model.
+    - This is used to take the pre-processed input text data and generate contextualized 
+      representations for the input text.
+    - As indicated by L-12_H-768_A-12, this model has 12 layers, a hidden size of 768, and
+      an attention head size of 12.
+"""
+
+# Define the BERT pre-processing model and the encoder
+tfhub_handle_preprocess = "https://tfhub.dev/tensorflow/bert_en_uncased_preprocess/3"
+tfhub_handle_encoder = "https://tfhub.dev/tensorflow/small_bert/bert_en_uncased_L-4_H-512_A-8/1"
+
+# Load the BERT pre-processing model and the encoder
+preprocessor = hub.KerasLayer(tfhub_handle_preprocess)
+encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True)
+
+# %%
+# 7. Create the BERT Classification Modle:
+# -------------------------------------------------------------------------------------------------
+def build_classifier_model():
+    """
+    Builds a classifier model using BERT for text classification.
+
+    Returns:
+        tf.keras.Model: The built classifier model.
+
+    This function builds a classifier model using BERT for text classification. It takes in a string input
+    and preprocesses it using the BERT preprocessing model. Then, it encodes the preprocessed input
+    using the BERT encoder. The encoded output is passed through a dropout layer and a dense layer with
+    a single output unit. The output of the dense layer is the final output of the classifier model.
+
+    The input to the model is a string tensor of shape `(None,)`, where `None` represents the batch size.
+    The output of the model is a tensor of shape `(None, 1)`, where `None` represents the batch size.
+
+    Note:
+        - The BERT preprocessing model and encoder are loaded from the specified URLs.
+        - The dropout layer has a dropout rate of 0.1.
+        - The dense layer has a single output unit with no activation function.
+    """
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+    preprocessing_layer = hub.KerasLayer(tfhub_handle_preprocess, name='preprocessing')
+    encoder_inputs = preprocessing_layer(text_input)
+    encoder = hub.KerasLayer(tfhub_handle_encoder, trainable=True, name='BERT_encoder')
+    outputs = encoder(encoder_inputs)
+    net = outputs['pooled_output']
+    net = tf.keras.layers.Dropout(0.1)(net)
+    net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
+    return tf.keras.Model(text_input, net)
+
+# Create the classifier model and print its summary
+classifier_model = build_classifier_model()
+classifier_model.summary()
+
+# %%
+
 # %%
